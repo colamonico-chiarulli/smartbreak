1863b93a74fb2412a6cce5bbfe9799b06f2f7bfc
Rino Andriano <andriano@colamonicochiarulli.it>
2021-04-13 12:34:19 +0200

    fix: Media Table JSON->LONGTEXT for compatibility

--- database/migrations/2021_04_11_130825_create_media_table.php
+++ database/migrations/2021_04_11_130825_create_media_table.php
@@ -1,4 +1,51 @@
 <?php
+/**
+ * File:	/database/migrations/2021_04_11_130825_create_media_table.php
+ * @package smartbreak
+ * @author  Giovanni Ciriello <giovanni.ciriello.5@gmail.com>
+ * @copyright	(c)2021 IISS Colamonico-Chiarulli Acquaviva delle Fonti (BA) Italy
+ * Created Date: Tuesday, April 11th 2021, 12:32:44 pm
+ * -----
+ * Last Modified: 	April 13th 2021 12:33:09 pm
+ * Modified By: 	Rino Andriano <andriano@colamonicochiarulli.it>
+ * -----
+ * @license	https://www.gnu.org/licenses/agpl-3.0.html AGPL 3.0
+ * ------------------------------------------------------------------------------
+ * SmartBreak is a School Bar food booking web application 
+ * developed during the PON course "The AppFactory" 2020-2021 with teachers 
+ * & students of "Informatica e Telecomunicazioni" 
+ * at IISS "C. Colamonico - N. Chiarulli" Acquaviva delle Fonti (BA)-Italy
+ * Expert dr. Giovanni Ciriello <giovanni.ciriello.5@gmail.com>
+ * ----------------------------------------------------------------------------
+ * SmartBreak is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by
+ * the Free Software Foundation
+ * 
+ * SmartBreak is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+ * details.
+ * You should have received a copy of the GNU Affero General Public License along 
+ * with this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ * 
+ * The interactive user interfaces in original and modified versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ * 
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the SmartBreak
+ * logo and IISS "Colamonico-Chiarulli" copyright notice. If the display of the logo
+ * is not reasonably feasible for technical reasons, the Appropriate Legal Notices 
+ * must display the words
+ * "(C) IISS Colamonico-Chiarulli-https://colamonicochiarulli.it - 2021".
+ * 
+ * ------------------------------------------------------------------------------
+ */
+
+?>
+<?php
 
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
@@ -20,10 +67,14 @@ class CreateMediaTable extends Migration
             $table->string('disk');
             $table->string('conversions_disk')->nullable();
             $table->unsignedBigInteger('size');
-            $table->json('manipulations');
-            $table->json('custom_properties');
-            $table->json('generated_conversions');
-            $table->json('responsive_images');
+            //$table->json('manipulations');
+            //$table->json('custom_properties');
+            //$table->json('generated_conversions');
+            //$table->json('responsive_images');
+            $table->longText('manipulations');
+            $table->longText('custom_properties');
+            $table->longText('generated_conversions');
+            $table->longText('responsive_images');
             $table->unsignedInteger('order_column')->nullable();
 
             $table->nullableTimestamps();
