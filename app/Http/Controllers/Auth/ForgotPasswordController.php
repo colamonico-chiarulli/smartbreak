d9332bf57bc7252656e3f8bda8794320dc2a6dc1
Giovanni Ciriello <giovanni.ciriello.5@gmail.com>
2021-05-09 10:29:01 +0200

    reset della password abilitato solo per manager e admin

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -6,41 +6,41 @@
  * @copyright	(c)2021 IISS Colamonico-Chiarulli Acquaviva delle Fonti (BA) Italy
  * Created Date: 	December 15th, 2020 11:05pm
  * -----
- * Last Modified: 	
- * Modified By: 	
+ * Last Modified:
+ * Modified By:
  * -----
  * @license	https://www.gnu.org/licenses/agpl-3.0.html AGPL 3.0
  * ------------------------------------------------------------------------------
- * SmartBreak is a School Bar food booking web application 
- * developed during the PON course "The AppFactory" 2020-2021 with teachers 
- * & students of "Informatica e Telecomunicazioni" 
+ * SmartBreak is a School Bar food booking web application
+ * developed during the PON course "The AppFactory" 2020-2021 with teachers
+ * & students of "Informatica e Telecomunicazioni"
  * at IISS "C. Colamonico - N. Chiarulli" Acquaviva delle Fonti (BA)-Italy
  * Expert dr. Giovanni Ciriello <giovanni.ciriello.5@gmail.com>
  * ----------------------------------------------------------------------------
  * SmartBreak is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by
  * the Free Software Foundation
- * 
+ *
  * SmartBreak is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
  * details.
- * You should have received a copy of the GNU Affero General Public License along 
+ * You should have received a copy of the GNU Affero General Public License along
  * with this program; if not, see http://www.gnu.org/licenses or write to the Free
  * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301 USA.
- * 
+ *
  * The interactive user interfaces in original and modified versions
  * of this program must display Appropriate Legal Notices, as required under
  * Section 5 of the GNU Affero General Public License version 3.
- * 
+ *
  * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the SmartBreak
  * logo and IISS "Colamonico-Chiarulli" copyright notice. If the display of the logo
- * is not reasonably feasible for technical reasons, the Appropriate Legal Notices 
+ * is not reasonably feasible for technical reasons, the Appropriate Legal Notices
  * must display the words
  * "(C) IISS Colamonico-Chiarulli-https://colamonicochiarulli.it - 2021".
- * 
+ *
  * ------------------------------------------------------------------------------
  */
 
@@ -51,6 +51,10 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Validation\ValidationException;
+use App\Models\User;
 
 class ForgotPasswordController extends Controller
 {
@@ -66,4 +70,33 @@ class ForgotPasswordController extends Controller
     */
 
     use SendsPasswordResetEmails;
+
+    /*
+     * Rule::unique('users')->where(function ($query) {
+                return $query->where('role', 'STUDENT');
+            })
+     */
+
+    public function sendResetLinkEmail(Request $request)
+    {
+
+        $this->validateEmail($request);
+
+        if(User::where('email', $request->email)->first()->role === 'STUDENT'){
+            throw ValidationException::withMessages([
+                'email' => ['Non sei abilitato a resettare la password'],
+            ]);
+        }
+
+        // We will send the password reset link to this user. Once we have attempted
+        // to send the link, we will examine the response then see the message we
+        // need to show to the user. Finally, we'll send out a proper response.
+        $response = $this->broker()->sendResetLink(
+            $this->credentials($request)
+        );
+
+        return $response == Password::RESET_LINK_SENT
+            ? $this->sendResetLinkResponse($request, $response)
+            : $this->sendResetLinkFailedResponse($request, $response);
+    }
 }
