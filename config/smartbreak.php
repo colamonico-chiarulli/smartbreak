77c2b23bae2ce0ddda6fb25a0afa2fcb1e03defd
Rino Andriano <andriano@colamonicochiarulli.edu.it>
2023-11-26 09:08:38 +0100

    Added check ENV for max_units_ordable

--- config/smartbreak.php
+++ config/smartbreak.php
@@ -6,12 +6,13 @@
  * @copyright	(c)2021 IISS Colamonico-Chiarulli Acquaviva delle Fonti (BA) Italy
  * Created Date: Tuesday, April 12th 2021, 12:39:38 pm
  * -----
- * Last Modified: 	October 20th 2022 7:57:43 pm
+ * Last Modified: 	November 26th 2023 9:04:43 pm
  * Modified By: 	Rino Andriano <andriano@colamonicochiarulli.edu.it>
  * -----
  * HISTORY:
  * Date      	By           	Comments
  * ----------	-------------	----------------------------------
+ * 2023-11-26	Rino Andriano	1.2 Added check ENV for max_units_ordable
  * 2022-10-20	Rino Andriano	1.1 Added check ENV for FROM-TO order-time-range
  * 2021-04-12	G. Ciriello	    First release 
  * -----
@@ -56,7 +57,7 @@
 
 return [
     // Max items orderable for each product
-    'max_units_ordable' => 5,
+    'max_units_ordable' => env('MAX_UNITS_ORDABLE', 3),
 
     'security_token' => env('SECURITY_CRON_TOKEN', 'afeff6005ab6ad9cd32b0b38492fcfd2'),
 
