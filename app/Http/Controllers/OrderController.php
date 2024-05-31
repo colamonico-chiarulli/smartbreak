c46ce3c305efc1326dd26a622cfb0d5d11b90431
Rino Andriano <andriano@colamonicochiarulli.edu.it>
2024-05-31 23:14:24 +0200

    New: feature order reBuy

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * File:	/app/Http/Controllers/OrderController.php
  * @package smartbreak
@@ -6,12 +7,14 @@
  * @copyright	(c)2021 IISS Colamonico-Chiarulli Acquaviva delle Fonti (BA) Italy
  * Created Date: 	February 27th, 2021 12:06pm
  * -----
- * Last Modified: 	October 21st 2022 12:03:03 am
- * Modified By: 	Giuseppe Giorgio <giuseppe.giorgio.inf@colamonicochiarulli.edu.it>
+ * Last Modified: 	March 31th 2024 20:03:03 pm
+ * Modified By: 	Rino Andriano <andriano@colamonicochiarulli.edu.it>
  * -----
  * HISTORY:
  * Date      	By           	Comments
  * ----------	-------------	----------------------------------
+ * 2024-05-31   R. Andriano     rebuy Feature (completed)
+ * 2022-12-03   C. Vaira        rebuy Feature (partial)
  * 2022-10-20	G. Giorgio	    1.1 deleteOrder
  * 2021-05-21	R. Andriano	    Order Status message
  * 2022-04-27	R. Andriano	    Orders-byStudente pagination
@@ -62,7 +65,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Order;
 use App\Models\Product;
-
 use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\DB;
@@ -72,7 +74,7 @@ class OrderController extends Controller
 
     /**
      * getOrdersByDay
-     * Prepara la vista Ordini del giorno
+     * Prepare the view Today's Order
      * 
      * CALLED BY BAR MANAGER
      *
@@ -82,40 +84,43 @@ class OrderController extends Controller
      */
     public function getOrdersByDay($date = null)
     {
-       $date = $date ?: now()->toDateString();
-       $site_id = auth()->user()->site->id;
-        
-       $orders = DB::table('orders')
-               ->join('order_product', 'id', '=', 'order_id')
-               ->join('products', 'product_id', '=', 'products.id')
-               ->join('users', 'user_id', '=', 'users.id')
-               ->join('classes', 'class_id', '=', 'classes.id')
-               ->select('class_id', 'status',
-                        DB::raw('CONCAT(year,course,"-", section) as class_name'),
-                        'products.id', 'products.name',
-                        DB::raw('SUM(quantity) as quantity'),
-                        DB::raw('SUM(order_product.price * order_product.quantity) as total')
-                        )
-               ->where('users.site_id', $site_id)
-               ->whereDate('orders.created_at', $date)
-               ->groupBy('class_id', 'product_id')
-               ->get(); 
- 
-        //Ordina per classe e prodotto
+        $date = $date ?: now()->toDateString();
+        $site_id = auth()->user()->site->id;
+
+        $orders = DB::table('orders')
+            ->join('order_product', 'id', '=', 'order_id')
+            ->join('products', 'product_id', '=', 'products.id')
+            ->join('users', 'user_id', '=', 'users.id')
+            ->join('classes', 'class_id', '=', 'classes.id')
+            ->select(
+                'class_id',
+                'status',
+                DB::raw('CONCAT(year,course,"-", section) as class_name'),
+                'products.id',
+                'products.name',
+                DB::raw('SUM(quantity) as quantity'),
+                DB::raw('SUM(order_product.price * order_product.quantity) as total')
+            )
+            ->where('users.site_id', $site_id)
+            ->whereDate('orders.created_at', $date)
+            ->groupBy('class_id', 'product_id')
+            ->get();
+
+        //Sort by class_name and product_name
         $orders = $orders->SortBy([
-            ['class_name','asc'],
-            ['name','asc']
+            ['class_name', 'asc'],
+            ['name', 'asc']
         ]);
-        
-        //Raggruppa gli ordini per classe
-        $classes=$orders->groupBy('class_id')->toArray();
+
+        //Group orders by class
+        $classes = $orders->groupBy('class_id')->toArray();
 
         return view('pages.orders.orders-by-day', compact('classes', 'date'));
     }
-    
+
     /**
      *  getProductsByDay
-     *  Prepara la vista Ordini per Prodotto
+     *  Prepare the view Orders by product
      * 
      *  CALLED BY BAR MANAGER
      *
@@ -128,81 +133,91 @@ class OrderController extends Controller
         $date = $date ?: now()->toDateString();
         $site_id = auth()->user()->site->id;
 
-        // Prendo tutti gli ordini di oggi
+        // Get all today's orders
         $products = DB::table('orders')
-        ->join('order_product', 'id', '=', 'order_id')
-        ->join('products', 'product_id', '=', 'products.id')
-        ->join('users', 'user_id', '=', 'users.id')
-        ->select('products.id', 'products.name', 'order_product.price', 'num_items', 'default_daily_stock',
-                 DB::raw('SUM(quantity) as quantity'),
-                 DB::raw('SUM(order_product.price * order_product.quantity) as total')
-                 )
-        ->where('users.site_id', $site_id)
-        ->whereDate('orders.created_at', $date)
-        ->groupBy('product_id')
-        ->orderBy('name', 'asc')
-        ->get(); 
+            ->join('order_product', 'id', '=', 'order_id')
+            ->join('products', 'product_id', '=', 'products.id')
+            ->join('users', 'user_id', '=', 'users.id')
+            ->select(
+                'products.id',
+                'products.name',
+                'order_product.price',
+                'num_items',
+                'default_daily_stock',
+                DB::raw('SUM(quantity) as quantity'),
+                DB::raw('SUM(order_product.price * order_product.quantity) as total')
+            )
+            ->where('users.site_id', $site_id)
+            ->whereDate('orders.created_at', $date)
+            ->groupBy('product_id')
+            ->orderBy('name', 'asc')
+            ->get();
 
-        
+
         //dd($products);        
 
         return view('pages.orders.products-by-day', compact('products', 'date'));
     }
-    
-    
+
+
     /**
      * getOrdersOfTodayByClass()
      *
      * CALLED BY STUDENT 
-     * Recupera gli ordini del giorno effettuati nella classe dello studente connesso
-     *
+     * Retrieve today's orders in the classroom of the connected student
+     * 
      * @access	public
      * @return	mixed
      */
     public function getOrdersOfTodayByClass()
     {
         $class_id = auth()->user()->class->id;
-        $class_name= auth()->user()->class->name; 
+        $class_name = auth()->user()->class->name;
         $date = date('Y-m-d');
-        
+
         $orders = DB::table('orders')
-                ->join('order_product', 'id', '=', 'order_id')
-                ->join('products', 'product_id', '=', 'products.id')
-                ->join('users', 'user_id', '=', 'users.id')
-                ->select('user_id', 'users.last_name', 'users.first_name', 'products.id', 'products.name', 
-                         DB::raw('SUM(quantity) as quantity'),
-                         DB::raw('SUM(order_product.price * order_product.quantity) as total')
-                         )
-                ->whereDate('orders.created_at', $date)
-                ->where('class_id', $class_id)
-                ->groupBy('user_id', 'product_id')
-                ->get(); 
-  
-        //Ordina per cognome e nome
+            ->join('order_product', 'id', '=', 'order_id')
+            ->join('products', 'product_id', '=', 'products.id')
+            ->join('users', 'user_id', '=', 'users.id')
+            ->select(
+                'user_id',
+                'users.last_name',
+                'users.first_name',
+                'products.id',
+                'products.name',
+                DB::raw('SUM(quantity) as quantity'),
+                DB::raw('SUM(order_product.price * order_product.quantity) as total')
+            )
+            ->whereDate('orders.created_at', $date)
+            ->where('class_id', $class_id)
+            ->groupBy('user_id', 'product_id')
+            ->get();
+
+        //Sort by last_name and first_name
         $orders = $orders->SortBy([
-            ['last_name','asc'],
-            ['first_name','asc']
+            ['last_name', 'asc'],
+            ['first_name', 'asc']
         ]);
-        
-        //Raggruppa gli ordini per utente
-        $orders=$orders->groupBy('user_id')->toArray();
-        
-        //Recupera lo STATUS degli ordini della classe partendo da un eventuale ordine utente
-        //Se l'utente non ha fatto ordini, non riuscirà a visualizzere lo status
-        $status=Order::select('status')
-                ->where('user_id', auth()->user()->id)
-                ->whereDate('created_at', date('Y-m-d'))
-                ->get()
-                ->pluck('status');
-        
-        return view('pages.orders.orders-by-class', compact('status','orders','class_name'));
+
+        //Group orders by user
+        $orders = $orders->groupBy('user_id')->toArray();
+
+        //Retrieve the STATUS of the class orders starting from a possible user order
+        //If the user has not placed any orders, he will not be able to view the status
+        $status = Order::select('status')
+            ->where('user_id', auth()->user()->id)
+            ->whereDate('created_at', date('Y-m-d'))
+            ->get()
+            ->pluck('status');
+
+        return view('pages.orders.orders-by-class', compact('status', 'orders', 'class_name'));
     }
 
     /**
      * getOrdersByStudent()
      *
      * CALLED BY STUDENT 
-     * Recupera gli ordini dello studente connesso
+     * Retrive the connected user's orders
      *
      * @access	public
      * @return	mixed
@@ -210,31 +225,33 @@ class OrderController extends Controller
     public function getOrdersByStudent()
     {
         $user_id = auth()->user()->id;
-        
-        //recupera gli ordini dell'utente per data e prodotto
+
+        //Retrieve the user's orders by date and product
         $orders = DB::table('orders')
-                ->join('order_product', 'id', '=', 'order_id')
-                ->join('products', 'product_id', '=', 'products.id')
-                ->select(DB::raw('DATE(orders.created_at) as date_order'),
-                        'products.id', 'products.name as name',
-                         DB::raw('SUM(quantity) as quantity'),
-                         DB::raw('SUM(order_product.price * order_product.quantity) as total')
-                         )
-                ->where('user_id', $user_id)
-                ->groupBy('date_order','products.id')
-                ->orderBy('date_order', 'desc')
-                ->paginate(30);
+            ->join('order_product', 'id', '=', 'order_id')
+            ->join('products', 'product_id', '=', 'products.id')
+            ->select(
+                DB::raw('DATE(orders.created_at) as date_order'),
+                'products.id',
+                'products.name as name',
+                DB::raw('SUM(quantity) as quantity'),
+                DB::raw('SUM(order_product.price * order_product.quantity) as total')
+            )
+            ->where('user_id', $user_id)
+            ->groupBy('date_order', 'products.id')
+            ->orderBy('date_order', 'desc')
+            ->paginate(30);
+
+        //Group orders by date
+        $orders_by_day = $orders->groupBy('date_order');
         
-        //raggruppa gli ordini per data
-        $orders_by_day=$orders->groupBy('date_order');
-        
-        //Recupera lo STATUS degli ordini del giorno partendo da un eventuale ordine utente
-        $status=Order::select('status')
-                    ->where('user_id', $user_id)
-                    ->whereDate('created_at', date('Y-m-d'))
-                    ->get()
-                    ->pluck('status');
-        return view('pages.orders.orders-by-student', compact('status','orders','orders_by_day'));
+        //Retrieve the STATUS of the class orders starting from a possible user order
+        $status = Order::select('status')
+            ->where('user_id', $user_id)
+            ->whereDate('created_at', date('Y-m-d'))
+            ->get()
+            ->pluck('status');
+        return view('pages.orders.orders-by-student', compact('status', 'orders', 'orders_by_day'));
     }
 
     /**
@@ -246,56 +263,106 @@ class OrderController extends Controller
      * @param	request	$request	
      * @return	void
      */
-    public function setOrderStatus(Request $request){
+    public function setOrderStatus(Request $request)
+    {
         $class_id = $request->input('class_id');
         $status = $request->input('status');
-        if ($status == 'READY' or $status == 'INCOMPLETE'){
-            $orders=Order::join('users', 'users.id', '=', 'user_id')
+        if ($status == 'READY' or $status == 'INCOMPLETE') {
+            $orders = Order::join('users', 'users.id', '=', 'user_id')
                 ->where('class_id', $class_id)
                 ->whereDate('orders.created_at', date('Y-m-d'))
                 ->update(['status' => $status]);
-        }       
+        }
     }
 
-
     /**
-     * Cancella gli ordini del giorno di un utente
+     * deleteOrder
+     * CALLED BY STUDENT
+     * 
+     * Delete today's order placed by connected user
+     * @access	public
+     * @return	void
      */
     public function deleteOrder()
     {
         $date = date('Y-m-d');
         $user = auth()->user()->id;
-        
-        //ottengo i prodotti ordinati
+
+        //Retrive list of ordered products
         $order_product = DB::table('order_product')
             ->join('orders', 'order_id', '=', 'orders.id')
             ->where('user_id', $user)
             ->whereDate('orders.created_at', $date)
             ->get();
-        
-        //per ogni prodotto ordinato ripristino la giacenza
-        foreach ($order_product as $item){
+
+        //For each ordered product, restore the stock quantity
+        foreach ($order_product as $item) {
             $product = Product::find($item->product_id);
-            //$product->num_items = $product->num_items + $item->quantity;
-            //$product->save();
             $product->increment('num_items', $item->quantity);
         }
 
 
-        //cancella tutti i prodotti ordinati del giorno e dell'utente 
+        //Delete all products ordered today by the user
         $order_product = DB::table('order_product')
             ->join('orders', 'order_id', '=', 'orders.id')
             ->where('user_id', $user)
             ->whereDate('orders.created_at', $date)
             ->delete();
 
-        //cancella tutti gli ordini del giorno e dell'utente
+        //Delete all orders of the day by the user
         $orders = Order::where('user_id', $user)
-                ->whereDate('created_at', $date)
-                ->delete();
-        
+            ->whereDate('created_at', $date)
+            ->delete();
     }
-    
+
+    /**
+     * rebuyOrder
+     * CALLED BY STUDENT
+     * 
+     * Reorder the products ordered in a day and put them in the cart
+     * @access	public
+     * @param	request	$request
+     * @return	void
+     */
+    public function rebuyOrder(Request $request)
+    {
+        $date = $request->input('orderDate');
+        $user = auth()->user()->id;
+
+        //Retrieve the ordered products for the date from the user
+        $order_product = DB::table('order_product')
+            ->join('orders', 'order_id', '=', 'orders.id')
+            ->where('user_id', $user)
+            ->whereDate('orders.created_at', $date)
+            ->get();
+        
+        //Retrieve session variables
+        $cart=session()->get('cart');
+        $categ=session()->get('categ');
+
+        //foreach product insert it into the cart
+        foreach ($order_product as $item) {
+            $product_id = $item->product_id;
+            $product_quantity = $item->quantity;
+            $categ_id= Product::find($product_id)->category_id;
+            
+            //update cart session variable
+            if(isset($cart[$product_id])){
+                $cart[$product_id]+=$product_quantity;
+            } else{
+                $cart[$product_id]=$product_quantity;
+            } 
+            
+            //update categ session variable
+            if(isset($categ[$categ_id])){
+                $categ[$categ_id]+=$product_quantity;
+            } else{
+                $categ[$categ_id]=$product_quantity;
+            } 
+        }
+        //store session variable
+        session(['cart' => $cart]);
+        session(['categ' => $categ]);
+    }
+        
 }
-
-
